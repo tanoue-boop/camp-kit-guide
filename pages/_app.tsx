8967c9f3e18cc209c1400350b7f650fa2442cbc1
tanoue-boop <tanoue@mjo-style.com>
2026-04-22 17:02:33 +0900

    feat(top): setup next/font and CSS variables for design renewal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,21 @@ import Layout from "../components/layout/Layout";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { pageview, GA_TRACKING_ID } from "../lib/gtag";
+import { Noto_Sans_JP, Jost } from "next/font/google";
+
+const notoSansJp = Noto_Sans_JP({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-noto-sans-jp",
+  display: "swap",
+});
+
+const jost = Jost({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-jost",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -16,8 +31,10 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.events]);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={`${notoSansJp.variable} ${jost.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
